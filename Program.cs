05d2fcbfed13f41545205eddbfc60bd9e8e348bc
EvgeniySh77 <evgen.sh77@mail.ru>
2022-02-05 21:29:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_6_3_2
+{
+    class Program
+    {
+        class Bus
+        {
+            public int? Load;
+
+            public void PrintStatus()
+            {
+                if (Load.HasValue)
+                {
+                    Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Автобус пуст!");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Bus().PrintStatus();
+            new Bus { Load = 5 }.PrintStatus();
+            Console.ReadKey();
+        }
+    }
+}
